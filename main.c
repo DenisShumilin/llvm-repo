23e74cb47e38a32f66abac5875def247c1937489
Denis Shumilin <shumilin.d.a@gmail.com>
2012-10-27 16:01:58 +0400

    simple_test

---
+++ main.c
@@ -0,0 +1,19 @@
+
+int main()
+{
+    int var1 = 2010;
+    int var2 = 10;
+    int var3 = 0;
+
+    var3 = var1 + var2;
+    var3 = var1 - var2;
+    var3 = var1 * var2;
+    var3 = var1 / var2;
+
+    if (var3 > var2)
+    {
+        var2 = var3;
+    }
+
+    return 0;
+}
